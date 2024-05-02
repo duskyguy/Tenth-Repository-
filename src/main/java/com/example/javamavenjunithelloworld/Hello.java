9175ff806f002d2d2a942f05870f02aa9969a0ae
duskyguy <167993609+duskyguy@users.noreply.github.com>
2024-05-02 08:01:00 +0530

    Update Hello.java
    trying from githib

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
  */
 public class Hello {
 
-    static final String HELLO = "Hello mama! version 1, commit thru eclipse";
+    static final String HELLO = "samling the changes";
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
     private short times = 1;
 
